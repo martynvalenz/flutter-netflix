3fcac6c437c7898eac51f92ee1f7b7b7a7c244ea
Martín Valenzuela <martynvalenz@gmail.com>
2021-01-17 21:58:18 -0700

    first commit

---
+++ lib/widgets/responsive.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  final Widget desktop;
+
+  const Responsive({
+    Key key, 
+    @required this.mobile, 
+    this.tablet, 
+    @required this.desktop
+  }) : super(key: key);
+
+  static bool isMobile(BuildContext context) => MediaQuery.of(context).size.width < 769;
+  static bool isTablet(BuildContext context) => MediaQuery.of(context).size.width >= 769 && MediaQuery.of(context).size.width < 1120;
+  static bool isDesktop(BuildContext context) => MediaQuery.of(context).size.width >= 1120;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder:(context, constraints){
+        if(constraints.maxWidth >= 1120){
+          return desktop;
+        }
+        else if(constraints.maxWidth >= 769 && constraints.maxWidth < 1120){
+          return tablet ?? mobile;
+        }
+        else {
+          return mobile;
+        }
+      }
+    );
+  }
+}
